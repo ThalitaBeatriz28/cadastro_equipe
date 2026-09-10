a0413e32e08d93ff819fd696a3d4f42cfab2e304
Thalita <thalietec@gmail.com>
2026-09-10 12:01:54 -0300

    Feat: Cadastro da quantidade

--- cadastro_produto.py
+++ cadastro_produto.py
@@ -1,20 +1,34 @@
-def cadastro():
+def cadastroPROD():
     print("\nRegistro do produto:")
     while True:
         produto = input("Digite o nome do produto: ").strip().title()
-        if produto.replace(" ", "").isalpha():
+        if produto == "":
+            print("\nO produto precisa ser nomeado\n")
+        elif produto.replace(" ", "").isalpha():
             break
         else:
-            print("\nDigite APENAS texto!")
+            print("\nDigite APENAS texto!\n")
 
     while True:
         try:
             valor = float(input("Digite o valor unitário: ").strip().replace("," , "."))
             if valor < 0:
-                print("Digite apenas valores positivos e válidos")
+                print("\nDigite apenas valores positivos e válidos\n")
             else:
                 break
         except ValueError:
-            print("Digite apenas números")
+            print("\nDigite apenas números\n")
 
+    while True:
+        try:
+            qnt = int(input("Digite a quantidade: ").strip())
+            if valor < 0:
+                print("\nDigite apenas quantidades válidos\n")
+            else:
+                break
+        except ValueError:
+            print("\nDigite apenas quantidades válidas\n")
+
+
+cadastroPROD()
 
